83050b1102c97f94ccf50b3d2a9ed5863771825e
dexterteo4 <137589600+dexterteo4@users.noreply.github.com>
2024-10-28 15:49:03 +0800

    Added calling of save function during logout

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -64,6 +64,8 @@ public class Controller {
     }
 
     public void logout() {
+        saveManager.savePatients();
+        saveManager.saveStaffs();
         setCurrentUser(null);
         showLoginMenu();
     }
